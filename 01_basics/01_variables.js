3ee85864c6b2e1d0e42a9fdae92a352eb72de5ca
Subhasis1818 <subhasismohanty098@gmail.com>
2025-04-25 19:32:20 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId=144552 // variable it cant be changed
+let accountEmail="subasis@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+/* 
+Note :-
+Use only
+1.const 
+2.let
+
+(donot use var )
+
+
+
+*/
+
+// accountId = 2
+console.log(accountId);
+accountEmail="hc@gmail.com";
+accountPassword="2221111";
+accountCity="jaipur";
+console.log(accountEmail);
+
+
+//tabluar structure ................. syntax:- console.table()
+console.table([accountEmail, accountId, accountPassword , accountCity , accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict "; // treat all JS code as newer version
+let accountEmail="hcgt";
+let accountId=2245;
+// alert(3+3); we are using node js not browser
+let name="Subhasis"
+let age=18;
+let isloggedIn= true;
+let state;
+
+
+
+/*
+number => 2 to power 53
+bigint
+string =>""
+boolean => true/false
+null =>standalone value
+undefined =>
+
+symbol =>unique 
+
+
+
+_________objects______a
+
+
+*/
+console.log(typeof accountEmail);
+console.log(accountId);
+console.log(typeof accountEmail,accountId);
+
+
+
