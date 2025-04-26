c0e3f7bc43c7dd431f505462b4a4a7360697f6da
Subhasis1818 <subhasismohanty098@gmail.com>
2025-04-26 22:35:27 +0530

    Add files via upload

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+
+let score="33abc"; // intrger ->number
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let changeIntoNum= Number(score);
+console.log(changeIntoNum);
+
+
+// "33"=> 33
+// "33abc" => cannot be changed to a number (output NaN)
+// true => 1
+// false =>0
+
+// true;
+let isLoggedIn = "" // "hitesh" means => true    and       2. if Empty String "" then => false 
+let changedLoggedIn= Boolean(isLoggedIn);
+console.log(changedLoggedIn)
+
+//false
+let isLoggedIn2 = 0; // 0 means false
+let changedLoggedIn2= Boolean(isLoggedIn2);
+console.log(changedLoggedIn2)
+
+
+
+let someNumber =33;
+let StringNumber =String(someNumber)
+console.log(StringNumber)
+
+
+
+
